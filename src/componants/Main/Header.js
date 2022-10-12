6b455d5028b41dc7358dc01cf8bb0bcbb2d1d898
Shovon15 <shovonrnbd@gmail.com>
2022-10-12 20:41:55 +0600

    add nav toggler

--- src/componants/Main/Header.js
+++ src/componants/Main/Header.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Bars3Icon, CodeBracketIcon, XMarkIcon } from "@heroicons/react/24/solid";
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+    // console.log(toggle);
     return (
-        <div>
-            <nav className="flex justify-between space-x-10 bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 h-20 p-5">
-                <div>
-                    <h1 className="text-xl font-bold">
-                        <small className="">Learn</small>
-                        <small className=" text-2xl text-red-400">#</small>Code
-                    </h1>
-                </div>
-                <div className="font-bold text-xl ">
-                    <Link to="/" className="px-2 hover:text-slate-300">
-                        Topic
-                    </Link>
+        <nav className="flex justify-between items-center space-x-10 bg-gradient-to-r from-slate-400 via-slate-700 to-blue-400 h-20 ">
+            <div className="w-40 flex flex-nowrap  font-bold text-2xl ml-10 text-red-500">
+                <span>Learn</span>
+                <span>
+                    <CodeBracketIcon className="h-6 w-6"></CodeBracketIcon>
+                </span>
+                <span>Code</span>
+            </div>
+            <div
+                className={`md:flex md:static justify-center text-center py-5 absolute w-full duration-500 ease-in-out font-bold text-xl ${
+                    toggle ? "top-8" : "top-[-260px]"
+                }`}
+            >
+                <Link to="/" className="px-2 hover:text-slate-300">
+                    Topic
+                </Link>
 
-                    <Link to="/statistics" className="px-2 hover:text-slate-300">
-                        Statistics
-                    </Link>
-                    <Link to="/blog" className="px-2 hover:text-slate-300">
-                        Blog
-                    </Link>
-                </div>
-            </nav>
-        </div>
+                <Link to="/statistics" className="px-2 hover:text-slate-300">
+                    Statistics
+                </Link>
+                <Link to="/blog" className="px-2 hover:text-slate-300">
+                    Blog
+                </Link>
+            </div>
+            <div onClick={() => setToggle(!toggle)} className="h-8 w-8 text-slate-900 md:hidden cursor-pointer">
+                {toggle ? <XMarkIcon /> : <Bars3Icon />}
+            </div>
+        </nav>
     );
 };
 
