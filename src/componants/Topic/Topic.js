037935c4b023b94259a0ec9b59515b342f586459
Shovon15 <shovonrnbd@gmail.com>
2022-10-12 21:25:00 +0600

    style debuging home page

--- src/componants/Topic/Topic.js
+++ src/componants/Topic/Topic.js
@@ -9,15 +9,19 @@ const Topic = () => {
     const { data } = topic;
     return (
         <div className="bg-slate-600 py-12">
-            <div className="mx-24 bg-slate-400 p-6 flex w-1/3 rounded-lg mb-12">
-                <img className="w-36 rounded-full border-4 border-green-500" src={img} alt="person.jpg"></img>
+            <div className="mx-5 lg:mx-20 bg-slate-400 p-6 flex lg:w-1/3 rounded-lg mb-12">
+                <img
+                    className="w-28 h-28 lg:w-36 rounded-full border-4 border-green-500"
+                    src={img}
+                    alt="person.jpg"
+                ></img>
                 <div className="flex flex-col px-5 justify-center">
                     <h1 className="text-2xl text-start font-bold">John Doe</h1>
                     <h1 className=" text-start font-bold py-1">Web developer.</h1>
                     <h1 className="font-medium">Code is like humor. When you have to explain it, it’s bad.</h1>
                 </div>
             </div>
-            <div className="grid grid-cols-4 gap-5 mx-20 ">
+            <div className="grid grid-col-1 md:grid-col-2 lg:grid-cols-4 gap-5 mx-5 md:mx-20 ">
                 {data.map((data) => (
                     <Subject key={data.id} data={data}></Subject>
                 ))}
