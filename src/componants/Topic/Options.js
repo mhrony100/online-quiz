a42adc38bae7a86b4abe3678db0ecd566f738f2d
Shovon15 <shovonrnbd@gmail.com>
2022-10-12 15:59:03 +0600

    add show answer

--- src/componants/Topic/Options.js
+++ src/componants/Topic/Options.js
@@ -3,9 +3,8 @@ import React from "react";
 const Options = ({ option }) => {
     // console.log(option);
     return (
-        <div className="border-2 rounded-lg border-black p-5 m-3">
-            {/* <h1>{option}</h1> */}
-            <input type="radio" /> {option}
+        <div className="border-2 rounded-lg border-black p-5 m-3 bg-green-300 ">
+            <button>{option}</button>
         </div>
     );
 };
--- src/componants/Topic/Questions.js
+++ src/componants/Topic/Questions.js
@@ -1,16 +1,39 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { toast, ToastContainer } from "react-toastify";
 import Options from "./Options";
+import "react-toastify/dist/ReactToastify.css";
 
 const Questions = ({ questions }) => {
-    const { options, question, id, correctAnswer } = questions;
+    const { options, question, correctAnswer } = questions;
     const newQuestion = question.replace("<p>", "").replace("</p>", "");
-    // console.log(options);
+    console.log(questions);
     // for (const option of options) {
     // console.log(options.index);
     // }
+
+    const showAnswer = () => {
+        console.log(correctAnswer);
+        toast.success(`${correctAnswer}`, {
+            style: {
+                padding: "16px",
+                color: "#22C55E",
+            },
+            iconTheme: {
+                primary: "#22C55E",
+                secondary: "#FFFAEE",
+            },
+        });
+    };
+
     return (
         <div className=" w-2/3 mx-auto p-5">
-            <h1 className="text-center font-bold bg-slate-300 pt-5 rounded-t-lg">{newQuestion}</h1>
+            <ToastContainer position="top-right" reverseOrder={true}></ToastContainer>
+            <div className="flex justify-between rounded-t-lg bg-slate-300 pt-5 px-8">
+                <h1 className="text-center font-bold ">{newQuestion}</h1>
+                <FontAwesomeIcon onClick={showAnswer} icon={faEye} className="cursor-pointer"></FontAwesomeIcon>
+            </div>
             <div className="grid grid-cols-2 bg-slate-300 p-3 pb-5 rounded-b-lg">
                 {options.map((option, index) => (
                     <Options key={index} option={option}></Options>
